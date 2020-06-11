776ba4f613e53c61a4f74a5c0587407c26890199
Mandalorian <pablogarciafuentes0@gmail.com>
2020-06-11 10:19:25 +0200

    Solucionado el Detalle, segun si ponemos /1, /2, /3 en el navegador nos mostrará cada card individualmente.

--- src/component/detalle.js
+++ src/component/detalle.js
@@ -1,55 +1,20 @@
 import React from 'react';
 import Card from './card';
+import ListCard from '../data/dataCard';
 
 class Detalle extends React.Component{
-    state = {id: 0}
+    state = {id: this.props.match.params.id}
 
-    componentDidMount(){
-        let id = this.props.match.params.id;
-        console.log(this.props);
-        this.setState({id});
-    }
 
     render(){
-        const obj = [
-            {
-                titulo: 'titulo 1',
-                descripcion: 'descripcion 1',
-                alt: 'alt 1',
-                href: 'url',
-                img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1589453919.32381247.png',
-                boton: 'detalle',
-                id: 1
-            },
-            {
-                titulo: 'titulo 2',
-                descripcion: 'descripcion 2',
-                alt: 'alt 2',
-                href: 'url 2',
-                img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1585134414.83551180.png',
-                boton: 'detalle 2',
-                id: 2
-            },
-            {
-                titulo: 'titulo 3',
-                descripcion: 'descripcion 3',
-                alt: 'alt 3',
-                href: 'url 3',
-                img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1585134404.36187772.png',
-                boton: 'detalle 3',
-                id: 3
-            }
-        ]
-        const {id} = this.state;
-        let item = obj[id];
-        for (var i=0; i<obj.length; i++){
-            var o = obj[i];
-            if(o.id === id) item = o;
-        }
+        
+        let {id} = this.state;
+        let item = ListCard.find(element => element.id == id);
+
+
         return (
             <div>
                 <Card obj={item}/>
-                Detalle Noticia número: {id}
             </div>
         )
     }
---
+++ src/data/dataCard.js
@@ -0,0 +1,31 @@
+const ListCard = [
+    {
+        titulo: 'titulo 1',
+        descripcion: 'descripcion 1',
+        alt: 'alt 1',
+        href: 'url',
+        img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1589453919.32381247.png',
+        boton: 'detalle',
+        id: 1
+    },
+    {
+        titulo: 'titulo 2',
+        descripcion: 'descripcion 2',
+        alt: 'alt 2',
+        href: 'url 2',
+        img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1585134414.83551180.png',
+        boton: 'detalle 2',
+        id: 2
+    },
+    {
+        titulo: 'titulo 3',
+        descripcion: 'descripcion 3',
+        alt: 'alt 3',
+        href: 'url 3',
+        img: 'https://i01.appmifile.com/v1/MI_18455B3E4DA706226CF7535A58E875F0267/pms_1585134404.36187772.png',
+        boton: 'detalle 3',
+        id: 3
+    }
+]
+
+export default ListCard;
